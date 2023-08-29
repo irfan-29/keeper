e1ad114eb018313a530742ba52ed2fe4cbbb734e
unknown <mdirfan29pro@gmail.com>
2023-08-29 10:02:14 +0530

    let-8

---
+++ frontend/build/precache-manifest.28ccab265727e1aacf921cbc72279729.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "5daccca74b6ed5e82919",
+    "url": "/static/js/main.5daccca7.chunk.js"
+  },
+  {
+    "revision": "e7d16e260401fa3935ba",
+    "url": "/static/js/1.e7d16e26.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ea4775459d630465310baf9721158a44",
+    "url": "/index.html"
+  }
+];
